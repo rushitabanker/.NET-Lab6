ed66bd2b146fc1d01b9a0a10eb3c87a3fcd84326
rushitabanker <81922524+rushitabanker@users.noreply.github.com>
2022-02-22 22:39:31 +0530

    Add files via upload

---
+++ Assignment6/Code_1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_1
+{
+
+    class SelectionSort
+    {
+        static public void Sort<T>(IList<T> sortArray, Func<T, T, bool> comparision)
+        {
+            bool swapped = true;
+            do
+            {
+                int position, i, j;
+                swapped = false;
+                for (i = 0; i < sortArray.Count - 1; i++)
+                {
+                    position = i;
+                    for (j = i; j < sortArray.Count; j++)
+                    {
+                        if (comparision(sortArray[position], sortArray[j]))
+                        {
+                            position = j;
+                        }
+                    }
+
+                    if (position != i)
+                    {
+                        T temp = sortArray[i];
+                        sortArray[i] = sortArray[i + 1];
+                        sortArray[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+    }
+    public enum Designations
+    {
+        CEO = 4,
+        CFO = 10,
+        sde = 5,
+        ba = 2,
+        pm = 6
+    }
+
+    class Employee
+    {
+        int empid;
+        float salary;
+        public string name;
+        Designations designation;
+
+        public Employee(int EmpId, float Salary, string NAme, Designations design)
+        {
+            this.empid = EmpId;
+            this.salary = Salary;
+            this.name = NAme;
+            this.designation = design;
+
+        }
+
+        internal static bool CompareSalary(Employee e1, Employee e2)
+        {
+            return e1.salary < e2.salary;
+        }
+
+        internal static bool CompareNames(Employee e1, Employee e2)
+        {
+            if (e1.name.CompareTo(e2.name) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        internal static bool CompareDesignations(Employee e1, Employee e2)
+        {
+            return e1.designation < e2.designation;
+        }
+    }
+
+
+    public class TestGenericMethods
+    {
+        public static void Main(string[] args)
+        {
+            List<Employee> emplist = new List<Employee>(30);
+            emplist.Add(new Employee(1111, 250000, "Harsh", Designations.CFO));
+
+            emplist.Add(new Employee(0011, 12000000, "Darpan", Designations.CEO));
+
+            emplist.Add(new Employee(2001, 501000, "Aishwarya", Designations.sde));
+
+            emplist.Add(new Employee(2004, 20999, "Rushita", Designations.CEO));
+
+            emplist.Add(new Employee(4001, 78000, "Jay", Designations.sde));
+
+            emplist.Add(new Employee(6006, 900000, "Rahul", Designations.CFO));
+
+
+            SelectionSort.Sort<Employee>(emplist, Employee.CompareSalary);
+
+            foreach (Employee e1 in emplist)
+            {
+                Console.WriteLine(e1.name);
+            }
+        }
+    }
+}
---
+++ Assignment6/Code_2/Program.cs
@@ -0,0 +1,67 @@
+﻿class ATM
+{
+    public int account_no;
+    public String account_holder_name;
+    public double avaliable_balance;
+
+    public ATM(int acc_no, String acc_name, double ava_bal)
+    {
+        this.account_no = acc_no;
+        this.account_holder_name = acc_name;
+        this.avaliable_balance = ava_bal;
+    }
+}
+
+public delegate void WatchBalance(double n);
+
+class Program
+{
+
+    public static void checkBalance(double bal)
+    {
+        if (bal < 0)
+        {
+            Console.WriteLine("You are overdrawn");
+        }
+        else if (bal < 10)
+        {
+            Console.WriteLine("Your account balance is very low");
+        }
+        else if (bal < 100)
+        {
+            Console.WriteLine("Watch your spending carefully.");
+        }
+        else
+        {
+            Console.WriteLine("You have over $100 in your account");
+        }
+    }
+
+    static void Main()
+    {
+
+        WatchBalance watch = checkBalance;
+        ATM ac1 = new ATM(200012232, "Rushita", 100);
+        watch(ac1.avaliable_balance);
+        Console.WriteLine("Ms. " + ac1.account_holder_name + " your current balance is " + ac1.avaliable_balance);
+
+        Console.WriteLine("*********************");
+
+        ATM ac2 = new ATM(200012255, "Harsh", -400);
+        watch(ac2.avaliable_balance);
+        Console.WriteLine("Mr. " + ac2.account_holder_name + " your current balance is " + ac2.avaliable_balance);
+
+        Console.WriteLine("*********************");
+
+        ATM ac3 = new ATM(200012222, "Aishwarya", 10);
+        watch(ac3.avaliable_balance);
+        Console.WriteLine("Ms. " + ac3.account_holder_name + " your current balance is " + ac3.avaliable_balance);
+
+        Console.WriteLine("*********************");
+
+        ATM ac4 = new ATM(200012291, "Yakshi", 50);
+        watch(ac4.avaliable_balance);
+        Console.WriteLine("Ms. " + ac4.account_holder_name + " your current balance is " + ac4.avaliable_balance);
+
+    }
+}
